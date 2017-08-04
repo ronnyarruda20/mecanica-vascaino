2abd0c57b4fb64eea6f60002544239b7e26141a1
ronnyarruda20 <ronnyapinho@gmail.com>
2017-08-03 23:50:08 -0400

    inserido o css materialize

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/materialize.css';
+import './css/style.css';
+import img1 from './img/background1.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+     <img src={img1} alt="Unsplashed background img 1"></img>
+
     );
   }
 }
